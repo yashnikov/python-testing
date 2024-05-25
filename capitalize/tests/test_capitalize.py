7406a59d4361eae251654df1faf2ad2ae0f39fa8
Vladimir Yashnikov <yashnikov@yashnikov.com>
2024-05-25 12:26:01 +0300

    rewrite test functions to assert

--- capitalize/tests/test_capitalize.py
+++ capitalize/tests/test_capitalize.py
@@ -5,10 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все тесты пройдены!')
